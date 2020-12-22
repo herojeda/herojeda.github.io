41d8fa1e59a52ca2826309c3e6fc5b4f8cb6b613
Hernan Ojeda <hernan.ojeda@rappi.com>
2020-12-22 01:47:23 -0300

    Adding resume

--- assets/js/messages.js
+++ assets/js/messages.js
@@ -1,16 +1,22 @@
 var messages = {
 
     "en": {
+
+        // Menu
         "menu-home": "Home",
-        "menu-about": "About",
+        "menu-about": "About me",
         "menu-resume": "Resume",
         "menu-contact": "Contact",
         "menu-language": "Language",
-        "submenu-language-en":"English",
-        "submenu-language-es":"Spanish",
+        "submenu-language-en": "English",
+        "submenu-language-es": "Spanish",
+
+        // Hero
         "hero-begin": "I'm ",
         "hero-typed-items": "Developer, Entrepreneur, Tech enthusiast",
-        "section-title-about": "About",
+
+        // About me
+        "section-title-about": "About me",
         "about-brief": "Since I discovered in my teens that I could create technology using my knowledge, I found a great passion and a path to follow in my life. I love what I do and feel lucky to be able to enjoy it every day.",
         "about-field-birthday": "Birthday: ",
         "about-field-website": "Website: ",
@@ -22,11 +28,13 @@ var messages = {
         "about-birthday": "26 Jul 1990",
         "about-website": "https://herojeda.github.io/",
         "about-phone": "+54 11 68593299",
-        "about-location": "Bs As, Argentina",
+        "about-location": "BA, Argentina",
         "about-age": 30,
         "about-email": "her.ojeda2@gmail.com",
         "about-freelance": "Available",
         "about-fullstack-brief": "As a full stack developer I not only take into account the technology I want to work with, but also that it fits to solve the problems and needs of the business. In order to have this understanding, my strategy is to get involved with the people who handle the product and with the final customer of the solution, in this way we can really determine what the product needs to solve customer problems.",
+
+        // Contact
         "section-title-contact": "Contact",
         "contact-brief": "If you are interested in my profile, if you think I can help build your solutions or if you simply want to send me a comment, I will leave you my contact information: ",
         "contact-field-location": "Location",
@@ -35,18 +43,67 @@ var messages = {
         "contact-email": "her.ojeda2@gmail.com",
         "contact-field-phone": "Call & WhatsApp",
         "contact-phone": "+54 11 68593299",
+
+        // Resume
+        "section-resume-title": "Resume",
+        "section-resume-brief": "",
+        "resume-summary-title": "Sumary",
+        "resume-summary": "Creative and objective-driven developer with 6+ years of experience in software development industry generating solutions that allow meeting deadlines without neglecting the quality and scalability of the product.",
+        "resume-address": "D. F. Sarmiento 1415, San Miguel, BA",
+        "resume-phone": "+54 11 68593299",
+        "resume-email": "her.ojeda2@gmail.com",
+        "resume-education-title": "Education",
+        "resume-education-degree": "Personal and professional computer technician",
+        "resume-education-institute": "Technical School No. 1, Muñiz, BA",
+        "resume-education-brief": "The Professional and Personal Computer Technician is trained to assist the user of computer products and services by providing installation services, training, systematization, primary maintenance, resolution of problems derived from the operation, and support for the contracting of computer products or services, developing the activities described in their professional profile and being able to act as a link between the specialist or expert in the subject, product or service and the end user.",
+        "resume-exp-title": "Professional Experience",
+
+        "resume-exp-1-title": "Software Developer",
+        "resume-exp-1-company": "IT Resources, Tech District, BA",
+        "resume-exp-1-desc-1": "Development and integration of new features for the Home Banking of the Santander Bank",
+        "resume-exp-1-desc-2": "Bug resolution, support and evolutionary maintenance",
+        "resume-exp-1-desc-3": "Administration and integration of features to the redesign of the Home Banking UI",
+        "resume-exp-1-desc-4": "Technologies: Java7 - Spring - JS - Jquery - Ajax - CSS - Html",
+
+        "resume-exp-2-title": "Software Engineer",
+        "resume-exp-2-company": "Sysone, Puerto Madero, BA",
+        "resume-exp-2-desc-1": "Development of products related to the insurance business for top-level companies: Sura, Galicia Seguros, Sancor Seguros",
+        "resume-exp-2-desc-2": "B2B Integration",
+        "resume-exp-2-desc-3": "Migration from monolithic architecture to microservices oriented architecture",
+        "resume-exp-2-desc-4": "Team leader in charge of the development and implementation of the endorsement module (4 people in charge)",
+        "resume-exp-2-desc-5": "Technologies: Java8 - Springboot - Hibernate - Primefaces",
+
+        "resume-exp-3-title": "Software Engineer",
+        "resume-exp-3-company": "Mercado Libre, Tech District, BA",
+        "resume-exp-3-desc-1": "Development of services in charge of the invoice generation, managing current accounts and accounting integrations for transactions related to sellers and financial products (Mercado Crédito)",
+        "resume-exp-3-desc-2": "Responsible for taking technical definitions based on business needs",
+        "resume-exp-3-desc-3": "Responsible for the migration of legacy services (Groovy -> Java11) related to the generation of charges to be billed",
+        "resume-exp-3-desc-4": "Technologies: Java11 - Groovy - Springboot - Spark - Clean Architecture - Event Driven Architecture - Cassandra - Oracle - Teradata",
+
+        "resume-exp-4-title": "Technical Lead",
+        "resume-exp-4-company": "Rappi, Nuñez, Bs. As.",
+        "resume-exp-4-desc-1": "Leading the design, development and implementation of integration services of financial movements from the platform to electronic invoicing, reconciliation of movements and accounting services, including tax calculation, integration of tax data, file storage and transactional consistency control",
+        "resume-exp-4-desc-2": "Delegate tasks to the 5 members of the development team and provide counsel on all aspects of the initiatives",
+        "resume-exp-4-desc-3": "Promote the joint development of the team maintaining and raising quality standards with each iteration",
+        "resume-exp-4-desc-4": "Technologies: Java11 - Kotlin - Vertx - RxJava - Spark - Clean Architecture - Event Driven Architecture - Kafka - S3 - Redis - PostgreSQL - Snowflake - Periscope - Python - Airflow - Typescript - Angular",
     },
 
     "es": {
+
+        // Menu
         "menu-home": "Inicio",
         "menu-about": "Sobre mi",
         "menu-resume": "Curriculum",
         "menu-contact": "Contacto",
         "menu-language": "Idioma",
-        "submenu-language-en":"Inglés",
-        "submenu-language-es":"Español",
+        "submenu-language-en": "Inglés",
+        "submenu-language-es": "Español",
+
+        // Hero
         "hero-begin": "Yo soy un ",
         "hero-typed-items": "Desarrollador de Software, Emprendedor, Entusiasta tecnológico",
+
+        // About me
         "section-title-about": "Sobre mi",
         "about-brief": "Desde que en mi adolescencia descubrí que podía crear tecnología usando mi conocimiento encontré una gran pasión y un camino a seguir en mi vida. Amo lo que hago y me siento afortunado de poder disfrutarlo todos los días.",
         "about-field-birthday": "Fecha de nacimiento: ",
@@ -64,14 +121,59 @@ var messages = {
         "about-email": "her.ojeda2@gmail.com",
         "about-freelance": "Disponible",
         "about-fullstack-brief": "Como desarrollador full stack no solo tengo en cuenta la tecnología con la que quiero trabajar, sino también que esta se ajuste para resolver los problemas y necesidades del negocio. Para tener este entendimiento mi estrategia es involucrarme con las personas que manejan el producto y con el cliente final de la solución, de esta forma podemos determinar realmente lo que el producto necesita para resolver los problemas de los clientes.",
+
+        // Contact
         "section-title-contact": "Contacto",
-        "contact-brief": "Si les resulta interesenta mi perfil, si creen que puedo ayudar a construir sus soluciones o si simplemente quieren enviarme algún comentario, les dejo mis datos de contacto: ",
+        "contact-brief": "Si les resulta interesente mi perfil, si creen que puedo ayudar a construir sus soluciones o si simplemente quieren enviarme algún comentario, les dejo mis datos de contacto: ",
         "contact-field-location": "Ubicación",
         "contact-location": "Domingo Faustino Sarmiento 1415, B1663 San Miguel, Provincia de Buenos Aires",
         "contact-field-email": "Email",
         "contact-email": "her.ojeda2@gmail.com",
         "contact-field-phone": "Llamadas y WhatsApp",
         "contact-phone": "+54 11 68593299",
+
+        // Resume
+        "section-resume-title": "Curriculum",
+        "section-resume-brief": "",
+        "resume-summary-title": "Resumen",
+        "resume-summary": "Desarrollador creativo e impulsado por el cumplimiento de los objetivos con más de 6 años de experiencia en la industria del desarrollo de software generando soluciones que permitan cumplir con los plazos sin dejar de lado la calidad y la escalabilidad del producto.",
+        "resume-address": "D. F. Sarmiento 1415, San Miguel, Bs. As",
+        "resume-phone": "+54 11 68593299",
+        "resume-email": "her.ojeda2@gmail.com",
+        "resume-education-title": "Educación",
+        "resume-education-degree": "Técnico en informática personal y profesional",
+        "resume-education-institute": "Escuela Técnica No. 1, Muñiz, Bs. As.",
+        "resume-education-brief": "El Técnico en Informática Profesional y Personal está capacitado para asistir al usuario de productos y servicios informáticos brindándole servicios de instalación, capacitación, sistematización, mantenimiento primario, resolución de problemas derivados de la operatoria, y apoyo a la contratación de productos o servicios informáticos, desarrollando las actividades descriptas en su perfil profesional y pudiendo actuar de nexo entre el especialista o experto en el tema, producto o servicio y el usuario final.",
+        "resume-exp-title": "Experiencia profesional",
+
+        "resume-exp-1-title": "Desarrollador de Software",
+        "resume-exp-1-company": "IT Resources, Distrito Tecnológico, Bs. As.",
+        "resume-exp-1-desc-1": "Desarrollo e integración de nuevas funcionalidades para el producto Home Banking del Banco Santander",
+        "resume-exp-1-desc-2": "Resolución de bugs, soporte y mantenimiento evolutivo",
+        "resume-exp-1-desc-3": "Administración e integración de funcionalidades relacionados al rediseño de la UI del Home Banking",
+        "resume-exp-1-desc-4": "Tecnologías: Java7 - Spring - JS - Jquery - Ajax - CSS - Html",
+
+        "resume-exp-2-title": "Ingeniero de Software",
+        "resume-exp-2-company": "Sysone, Puerto Madero, Bs. As.",
+        "resume-exp-2-desc-1": "Desarrollo de productos relacionados al negocio de seguros para companías de primer nivel: Sura, Galicia Seguros, Sancor Seguros",
+        "resume-exp-2-desc-2": "Integración B2B",
+        "resume-exp-2-desc-3": "Migración desde arquitectura monolítica hacia arquitectura orientada a microservicios",
+        "resume-exp-2-desc-4": "Lider de equipo encargado del desarrollo e implementación del módulo de endosos (4 personas a cargo)",
+        "resume-exp-2-desc-5": "Tecnologías: Java8 - Springboot - Hibernate - SQL Data Bases - Primefaces",
+
+        "resume-exp-3-title": "Ingeniero de Software",
+        "resume-exp-3-company": "Mercado Libre, Distrito Tecnológico, Bs. As.",
+        "resume-exp-3-desc-1": "Desarrollo de servicios responsables de generar facturación, administrar cuentas corrientes e integrar contablemente todos los movimientos relacionados a vendedores y productos financieros (Mercado Crédito)",
+        "resume-exp-3-desc-2": "Responsable de tomar definiciones técnicas a partir de la necesidades del negocio",
+        "resume-exp-3-desc-3": "Responsable de la migración de servicios legados (Groovy -> Java11) relacionados la generación de los cargos a facturar",
+        "resume-exp-3-desc-4": "Tecnologías: Java11 - Groovy - Springboot - Spark - Clean Architecture - Event Driven Architecture - Cassandra - Oracle - Teradata",
+
+        "resume-exp-4-title": "Lider Técnico",
+        "resume-exp-4-company": "Rappi, Nuñez, Bs. As.",
+        "resume-exp-4-desc-1": "Liderar el diseño, desarrollo e implementación de los servicios de integración de movimientos financieros desde la plataforma hacia facturación electrónica, conciliación de movimientos y servicios contables, incluyendo cálculo de impuestos, integración de datos fiscales, almacenamiento de archivos y control de consistencia transaccional.",
+        "resume-exp-4-desc-2": "Delegar tareas a los 5 miembros del equipo de desarrollo y brindar asesoramiento sobre todos los aspectos de las iniciativas",
+        "resume-exp-4-desc-3": "Promover el desarrollo conjunto del equipo manteniendo y elevando los estándares de calidad con cada iteración",
+        "resume-exp-4-desc-4": "Tecnologías: Java11 - Kotlin - Vertx - RxJava - Spark - Clean Architecture - Event Driven Architecture - Kafka - S3 - Redis - PostgreSQL - Snowflake - Periscope - Python - Airflow - Typescript - Angular",
     },
 
-};
+}
